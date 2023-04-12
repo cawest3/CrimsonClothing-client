7e921fd9b1a54278ae919d142d2762ad9d99a85d
Jared Burnham <jaredburnham@Jareds-MacBook-Pro.local>
2023-04-12 15:38:54 -0500

    js changes

--- Customer HTMLs/resources/scripts/custprofile.js
+++ Customer HTMLs/resources/scripts/custprofile.js
@@ -7,6 +7,7 @@ let activeUser = JSON.parse(localStorage.getItem("activeUser"))
 
 function setUpUser(){
     activeUser = {
+        userId: 1,
         name: 'Jonathan Gaming',
         email: 'jonathangaming@epic.com',
         address: "1234 Main Street, Mobile, AL 36609"
@@ -29,3 +30,47 @@ function handleOnLoad(){
     let address = document.getElementById("address")  //needs to be added
     address.value = activeUser.address
 }
+
+function editProfile(){
+    let currUserId = activeUser.userId
+    let name = document.getElementById("name").value//needs to be added
+    let email = document.getElementById("email").value//or username, depending
+    let address = document.getElementById("address").value  //needs to be added
+
+    console.log(name, email, address, currUserId)
+    if (confirm(`Is this correct? Name: ${name}, Email: ${email}, Address: ${address}`)){
+        console.log('confirmed')
+        updateUser(currUserId)
+    } else{
+        return false
+    }
+}
+
+function updateUser(userId){
+    let putUserApiUrl = `https://localhost:7050/api/Customer/${userId}`
+    fetch (putUserApiUrl, {
+        method: "PUT",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            userId: userId,
+            username: email,
+            password: activeUser.password,
+            name: name,
+            address: address,
+            deleted: song.deleted
+        })
+    })
+    .then((response) =>{
+        console.log(response)
+    })
+}
+
+// async function findUser(currUserId){
+//     let songs = await getUsers()
+//     let returnVal = songs.find(x => x.userId == currUser)
+//     console.log(returnVal)
+//     return returnVal
+// }
