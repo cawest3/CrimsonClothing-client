04f51a308df5781fe17f68c78314449890216de5
cweaver4 <112584272+cweaver4@users.noreply.github.com>
2023-04-17 15:31:15 -0500

    another one

--- Customer HTMLs/resources/scripts/custlogin.js
+++ Customer HTMLs/resources/scripts/custlogin.js
@@ -1,37 +1,50 @@
-const custUrl = ""
+const custUrl = "";
 
-async function handleOnCustLogin()
-{
-    GetCustLogins()
-}
- 
-async function GetCustLogins()
-{
-    try{
-        const response = await fetch(custUrl)
-        const data = await response.json()
-        custLogins = []
-        data.forEach((cLogin) => {
-            cLogin = {
-                username: cLogin.username,
-                password: cLogin.password,
-                }
-                cLogin.unshift(cLogin)
-            })
-            
-            localStorage.clear()
-            localStorage.setItem('customerLogins', JSON.stringify(custLogins))
-        }
-        catch{
-            console.log("error")
-        }
+async function handleOnCustLogin() {
+  await GetCustLogins();
 }
 
-async function CustomerLogin(username, password)
-{
-    const response = await
-    cLogin 
-    {
-        
+async function GetCustLogins() {
+  try {
+    const response = await fetch(custUrl);
+    const data = await response.json();
+    custLogins = [];
+    data.forEach((cLogin) => {
+      cLogin = {
+        username: cLogin.username,
+        password: cLogin.password,
+      };
+      custLogins.unshift(cLogin);
+    });
+
+    localStorage.clear();
+    localStorage.setItem("customerLogins", JSON.stringify(custLogins));
+  } catch {
+    console.log("error");
+  }
+}
+
+async function CustomerLogin(username, password) {
+  const custLogins = JSON.parse(localStorage.getItem("customerLogins"));
+  for (const cLogin of custLogins) {
+    if (cLogin.username === username && cLogin.password === password) {
+      return true;
     }
+  }
+  return false;
 }
+
+document.getElementById("login-form").addEventListener("submit", async function (event) {
+  event.preventDefault(); // Prevent the form from submitting by default
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (await CustomerLogin(email, password)) {
+    window.location.href = "./home.html"; // Redirect to the home page
+  } else {
+    alert("Invalid email or password. Please try again.");
+  }
+});
+
+handleOnCustLogin();
