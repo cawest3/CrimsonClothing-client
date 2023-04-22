38cd6c86190c7eef0c0b29a36b8c144015d7bc52
Caroline West <carolinewest@Carolines-MacBook-Air-2.local>
2023-04-22 13:28:34 -0500

    caroline

--- Admin HTML/resources/scripts/adminlogin.js
+++ Admin HTML/resources/scripts/adminlogin.js
@@ -5,113 +5,45 @@ let adminUrl = "http://localhost:5165/admin"
 let customerUrl = "http://localhost:5165/api/Customer"
 let aLogins = JSON.parse(localStorage.getItem("adminLogins")) ? JSON.parse(localStorage.getItem('adminLogins')) : []
 
-function HandleOnLoad()
-{
-  GetCustLogins();
+function HandleOnLoad() {
+  GetAdminLogins();
 }
- 
-async function GetCustLogins() {
+
+async function GetAdminLogins() {
   try {
     const response = await fetch(adminUrl);
     const data = await response.json();
     localStorage.clear();
-    localStorage.setItem('customerLogins', JSON.stringify(data));
+    localStorage.setItem('adminLogins', JSON.stringify(data));
     console.log(data);
   } catch {
     console.log("error");
   }
-} // Add missing closing brace
+}
 
 const aLoginForm = document.getElementById("Alogin-form");
-cLoginForm.addEventListener("submit", handleSubmit);
+aLoginForm.addEventListener("submit", handleSubmit);
 
 async function handleSubmit(event) {
   event.preventDefault();
 
-  const inputUsername = event.target.querySelector('input[name="email"]');
-  const inputPassword = event.target.querySelector('input[name="password"]');
+  const adminInputUsername = event.target.querySelector('input[name="email"]');
+  const adminInputPassword = event.target.querySelector('input[name="password"]');
   console.log('hi')
-  if (CustomerLogin(inputUsername, inputPassword)) {
-    
-    window.location.href = './shop.html' // Redirect to the home page
+  if (AdminLogin(adminInputUsername, adminInputPassword)) {
+    window.location.href = './reports.html' // Redirect to the home page
   } else {
     alert("Invalid email or password. Please try again.");
   }
 }
 
-async function CustomerLogin(inputUsername, inputPassword) {
-  const custLogins = JSON.parse(localStorage.getItem("customerLogins"));
-  for (const cLogin of custLogins) {
-    if (cLogin.custUsername === inputUsername.value && cLogin.password === inputPassword.value) {
+async function AdminLogin(adminInputUsername, adminInputPassword) {
+  const aLogins = JSON.parse(localStorage.getItem("adminLogins"));
+  for (const aLogin of aLogins) {
+    if (aLogin.adminUsername === adminInputUsername.value && aLogin.adminPassword === adminInputPassword.value) {
       return true;
     }
   }
   return false;
 }
-
-
-
-
-
-// const adminUrl = ""
-
-// async function HandleOnAdminLogin()
-// {
-//     GetAdminLogins()
-
-// }
- 
-// async function GetAdminLogins()
-// {
-//     try{
-//         const response = await fetch(adminUrl)
-//         const data = await response.json()
-//         adminLogins = []
-//         data.forEach((aLogin) => {
-//             aLogin = {
-//                 username: aLogin.username,
-//                 password: aLogin.password,
-//                 }
-//                 aLogin.unshift(aLogin)
-//             })
-            
-//             localStorage.clear()
-//             localStorage.setItem('theAdminLogins', JSON.stringify(adminLogins))
-//         }
-//         catch{
-//             console.log("error")
-//         }
-// }
-
-// async function AdminLogin(username, password) 
-// {
-//   const adminLogins = JSON.parse(localStorage.getItem("adminLogins"));
-//   for (const aLogin of adminLogins) 
-//   {
-//     if (aLogin.username === username && aLogin.password === password) 
-//     {
-//       return true;
-//     }
-//   }
-//   return false;
-// }
-
-//   document.getElementById("alogin-form").addEventListener("submit", async function (event) {
-//   event.preventDefault();  // Prevent the form from submitting by default
-
-//   const email = document.getElementById("email").value;
-//   const password = document.getElementById("password").value;
-// if (await AdminLogin(email, password)) 
-//   {
-//     window.location.href = "./home.html"; // Redirect to the home page
-//   } 
-//   else 
-//   {
-//     alert("Invalid email or password. Please try again.");
-//   }
-//   });
-
-
-
-
 
