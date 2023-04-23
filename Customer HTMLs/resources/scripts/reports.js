f61dd1d5420312b4dc32f5465e3511a2eef606e5
Jared Burnham <jmburnham@crimson.ua.edu>
2023-04-23 12:43:44 -0500

    Reports

--- Customer HTMLs/resources/scripts/reports.js
+++ Customer HTMLs/resources/scripts/reports.js
@@ -1,46 +1,56 @@
 async function getData() {
-    const allTransactionsApiUrl = ''
+    const allTransactionsApiUrl = 'http://localhost:5165/api/transaction'
     const response = await fetch(allTransactionsApiUrl)
     const data = await response.json()
     return data
 }
 
-//let data = getData()
+async function getData2() {
+    const allItemsApiUrl = 'http://localhost:5165/api/item'
+    const response = await fetch(allItemsApiUrl)
+    const data2 = await response.json()
+    return data2
+}
 
-let data = [
-    {
-        id: 1,
-        profit: 10
-    },
-    {
-        id: 1,
-        profit: 10
-    },
-    {
-        id: 1,
-        profit: 10
-    },
-    {
-        id: 1,
-        profit: 10
-    }
-]
+async function runReports(){
+    const data = await getData()
+    const data2 = await getData2()
+    transactionAmountReport(data)
+    totalProfitReport(data)
+    totalItemsReport(data2)
+}
 
-transactionAmountReport()
-totalProfitReport()
+// let data = getData()
 
-function transactionAmountReport(){
+
+// transactionAmountReport()
+// totalProfitReport()
+
+function transactionAmountReport(data){
     let transactionAmount = 0
     data.forEach(data =>{
         transactionAmount++
     })
     console.log(transactionAmount)
+    document.getElementById('totalTransacs').innerHTML = transactionAmount;
 }
 
-function totalProfitReport(){
+function totalProfitReport(data){
     let totalProfit = 0
     data.forEach(data =>{
         totalProfit += data.profit
     })
     console.log(totalProfit)
+    document.getElementById('totalProfit').innerHTML = totalProfit
 }
+
+function totalItemsReport(data2){
+    let totalItems = 0
+    data2.forEach(data2 =>{
+        totalItems++
+    })
+    console.log(totalItems)
+    document.getElementById('totalItems').innerHTML = totalItems
+}
+
+runReports()
