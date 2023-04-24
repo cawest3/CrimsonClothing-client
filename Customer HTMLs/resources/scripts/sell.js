7fb1046f51a544ef81053686f892fc1e42745cfb
Amanda Malosh <amandamalosh@Amandas-MacBook-Air-2.local>
2023-04-23 19:29:55 -0500

    new sell that doesnt work either

--- Customer HTMLs/resources/scripts/sell.js
+++ Customer HTMLs/resources/scripts/sell.js
@@ -1,135 +1,115 @@
-let itemUrl = "http://localhost:5165/item"
-let consignmentUrl = "http://localhost:5165/consignment"
-let transactionUrl = "http://localhost:5165/transaction"
-let adminUrl = "http://localhost:5165/admin"
-let customerUrl = "http://localhost:5165/Customer"
+let itemUrl = "http://localhost:5165/item";
+let consignmentUrl = "http://localhost:5165/consignment";
+let transactionUrl = "http://localhost:5165/transaction";
+let adminUrl = "http://localhost:5165/admin";
+let customerUrl = "http://localhost:5165/Customer";
 
-// const pricePerItem= 
-// {
-//     shirt: 5,
-//     pant : 5,
-//     skirt : 5,
-//     dress : 7,
-//     jacket : 10,
-//     shoe : 10,
-//     belt : 7,
-// };
+function calculateConsignment() {
+  const firstNameInput = document.getElementById('firstName').value;
+  const lastNameInput = document.getElementById('lastName').value;
+  const emailInput = document.getElementById('emailInput').value;
+  const typeInput = document.getElementById('itemType').value;
+  const sizeInput = document.getElementById('sizeInput').value;
+  const qtyInput = document.getElementById('qtyInput').value;
+  const pictureInput = document.getElementById('pictureUpload').value;
 
+  const price = findPrice(typeInput, qtyInput);
+  console.log(price);
 
-function CalculateConsignment()
-{
-    // Define the prices per item
-    const shirtInput = document.getElementById('shirt');
-    const pantsInput = document.getElementById('pant');
-    const shoesInput = document.getElementById('shoe');
-    const skirtInput = document.getElementById('skirt');
-    const beltInput = document.getElementById('belt');
-    const jacketInput = document.getElementById('jacket');
-    const dressInput = document.getElementById('dress');
-
-    // Get the values from the form
-    const shirtQuantity = Number(shirtInput.value);
-    const pantsQuantity = Number(pantsInput.value);
-    const shoesQuantity = Number(shoesInput.value);
-    const skirtQuantity = Number(skirtInput.value);
-    const beltQuantity = Number(beltInput.value);
-    const jacketQuantity = Number(jacketInput.value);
-    const dressQuantity = Number(dressInput.value);
-    
-    const consignment= [
-        {item:'shirt', quantity: shirtQuantity},
-        {item:'pant', quantity: pantsQuantity},
-        {item:'shoe', quantity: shoesQuantity},
-        {item:'skirt', quantity: skirtQuantity},
-        {item:'belt', quantity: beltQuantity},
-        {item:'jacket', quantity: jacketQuantity},
-        {item:'dress', quantity: dressQuantity},
-     ]
-     //should there be an event listener here
-     // price will log to console, quantity will not work 
-        let totalCost = 0;
-        for(let item of consignment)
-        {
-            const price = pricePerItem[item.item];
-            console.log(price)
-            const quantity = item.quantity;
-            console.log(quantity)
-            //get quantity to work
-            totalCost += price * 1;
-            console.log("Total cost")
-            console.log(totalCost)
-        }
-
-        
-        alert("The total value of the consignment is $" + totalCost.toFixed(2));
-  // Calculate the total cost of the consignment   
+  alert("The total value of the consignment is $" + price.toFixed(2));
 }
 
-function AddAnotherItem()
-{
-    console.log('hi')
-    const type =
-    {
-        'Shirt':'shirt',
-        'Pants' : 'pant',
-        'Skirts' : 'skirt',
-        'Dresses' : 'dress',
-        'Jackets' : 'jacket',
-        'Shoes' : 'shoes',
-        'Belt' : 'belt',
-    }
-    document.addEventListener('DOMContentLoaded', function(){
-        const typeDropdown = document.getElementById('type-Dropdown')
-        let form = document.createElement('form')
+function findPrice(type, qty) {
+  let price = 0;
+
+  if (type === 'shirt') {
+    price = 5 * qty;
+  } else if (type === 'pant') {
+    price = 5 * qty;
+  } else if (type === 'skirt') {
+    price = 5 * qty;
+  } else if (type === 'dress') {
+    price = 7 * qty;
+  } else if (type === 'jacket') {
+    price = 10 * qty;
+  } else if (type === 'shoe') {
+    price = 10 * qty;
+  } else if (type === 'belt') {
+    price = 7 * qty;
+  }
+
+  return price;
+}
+
+document.getElementById('calculateButton').addEventListener('click', calculateConsignment);
+
+
+
+// function AddAnotherItem()
+// {
+//     console.log('hi')
+//     const type =
+//     {
+//         'Shirt':'shirt',
+//         'Pants' : 'pant',
+//         'Skirts' : 'skirt',
+//         'Dresses' : 'dress',
+//         'Jackets' : 'jacket',
+//         'Shoes' : 'shoes',
+//         'Belt' : 'belt',
+//     }
+//     document.addEventListener('DOMContentLoaded', function(){
+//         const typeDropdown = document.getElementById('type-Dropdown')
+//         let form = document.createElement('form')
         
-        // for (let category in type)
-        // {
-        // let option = document.createElement("option")
-        // option.setAttribute('value', type[category])
-        // let optionText = document.createTextNode(category)
-        // option.appendChild(optionText)
-        // typeDropdown.appendChild(option)
-        // }
+//         // for (let category in type)
+//         // {
+//         // let option = document.createElement("option")
+//         // option.setAttribute('value', type[category])
+//         // let optionText = document.createTextNode(category)
+//         // option.appendChild(optionText)
+//         // typeDropdown.appendChild(option)
+//         // }
    
 
-    let textInput = document.createElement('input')
-    textInput.type = 'text'
-    textInput.placeholder = 'Enter Size of Item'
-    textInput.id = 'additionalSize'
-    form.appendChild(textInput)
+//     let textInput = document.createElement('input')
+//     textInput.type = 'text'
+//     textInput.placeholder = 'Enter Size of Item'
+//     textInput.id = 'additionalSize'
+//     form.appendChild(textInput)
 
-    let textInput1 = document.createElement('input')
-    textInput1.type = 'text'
-    textInput1.placeholder = 'Enter picture link'
-    textInput1.id = 'additionalPic'
-    form.appendChild(textInput1)
+//     let textInput1 = document.createElement('input')
+//     textInput1.type = 'text'
+//     textInput1.placeholder = 'Enter picture link'
+//     textInput1.id = 'additionalPic'
+//     form.appendChild(textInput1)
 
-    let submitButton = document.createElement('button')
-    submitButton.textContent = 'Submit'
-    form.appendChild(submitButton)
+//     let submitButton = document.createElement('button')
+//     submitButton.textContent = 'Submit'
+//     form.appendChild(submitButton)
 
-    let consignmentCount = '1'
-    form.addEventListener('submit', function(e){
-        e.preventDefault()
-        let consignment = {
-            consignmentId: consignmentCount ++,
-            size: e.target.elements.additionalSize.value,
-            pictureLink: e.target.elements.additionalPic.value,
+//     let consignmentCount = '1'
+//     form.addEventListener('submit', function(e){
+//         e.preventDefault()
+//         let consignment = {
+//             consignmentId: consignmentCount ++,
+//             size: e.target.elements.additionalSize.value,
+//             pictureLink: e.target.elements.additionalPic.value,
             
-        }
+//         }
         
-        addRow(consignment)
+//         addRow(consignment)
 
-        e.target.elements.additionalType = ''
-        e.target.elements.additionalSize = ''
-        e.target.elements.additionalPic = ''
+//         e.target.elements.additionalType = ''
+//         e.target.elements.additionalSize = ''
+//         e.target.elements.additionalPic = ''
 
-    })
+//     })
     
 
-    app.appendChild(form)
-});
-}
+//     app.appendChild(form)
+// });
+// }
 
 
 
