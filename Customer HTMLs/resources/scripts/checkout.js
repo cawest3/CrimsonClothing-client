4ab7f785ac4293d5990ef4c6c463028b94305651
Caroline West <carolinewest@Carolines-MacBook-Air-2.local>
2023-04-23 21:08:34 -0500

    checkout

--- Customer HTMLs/resources/scripts/checkout.js
+++ Customer HTMLs/resources/scripts/checkout.js
@@ -3,23 +3,50 @@ let consignmentUrl = "http://localhost:5165/consignment"
 let transactionUrl = "http://localhost:5165/transaction"
 let adminUrl = "http://localhost:5165/admin"
 let customerUrl = "http://localhost:5165/customer"
-//let checkoutCart = []
 let transactionProfit = 0;
-
-
-let activeUser = JSON.parse(localStorage.getItem("activeUser"))
-
+let activeUser = JSON.parse(localStorage.getItem("activeUser"));
 
 function HandleOnLoad(){
     loadItems()
-    SetUpUser()
-    
-}   
-
+}  
 
 function loadItems() {
-    const activeUser = JSON.parse(localStorage.getItem("activeUser"));
+   
     console.log(activeUser.cart)
+
+    const cartItemsContainer = document.querySelector(".items-container");
+    let innerHTML = "";
+    let subtotal = 0;
+
+    try {
+        cart.forEach((item) => {
+            subtotal += item.price;
+            if (item.inCart === true) {
+              innerHTML += `
+              <div class="col-md-4 order-md-2 mb-4">
+              <h4 class="d-flex justify-content-between align-items-center mb-3">
+                <span class="text-muted">Your cart</span>
+                <span class="badge badge-secondary badge-pill">3</span>
+              </h4>
+              <ul class="list-group mb-3">
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">Product name</h6>
+                    <small class="text-muted">Item Name</small>
+                  </div>
+                  <span class="text-muted">$12</span>
+                </li>
+                </ul>
+              `;
+            }
+          });
+        cartItemsContainer.innerHTML = innerHTML;
+    } catch (error) {
+        console.error("Error fetching cart items:", error);
+    }
+}
+
+
     // get reference to the shopping cart element
     const checkoutCart = document.getElementById('cart');
 
@@ -51,24 +78,7 @@ function loadItems() {
     // append the checkout item to the checkout element
     checkout.appendChild(checkoutItem);
 }
-}
 
-
-
-
-
-
-
-
-// function SetUpUser(){
-//     activeUser = {
-//         customerId: activeUser.customerId,
-//         username: activeUser.username,
-//         password: activeUser.password,
-//         cart: activeUser.cart
-//     }
-    
-// }
 
 
 // function HandleCheckCheckOutClick(){
@@ -137,45 +147,8 @@ function loadItems() {
         
 // }  
 
-//  function loadCartItems() {
-//     const cartItemsContainer = document.querySelector(".items-container");
-//     let innerHTML = "";
-//     let subtotal = 0;
-
-//     try {
-//         const response = fetch(transactionUrl);
-//         const cartItems = response.json();
-
-//         activeuser.cart.forEach((item) => {
-//             subtotal += item.price;
-//             if (item.inCart === true) {
-//               innerHTML += `
-//               <div class="col-md-4 order-md-2 mb-4">
-//               <h4 class="d-flex justify-content-between align-items-center mb-3">
-//                 <span class="text-muted">Your cart</span>
-//                 <span class="badge badge-secondary badge-pill">3</span>
-//               </h4>
-//               <ul class="list-group mb-3">
-//                 <li class="list-group-item d-flex justify-content-between lh-condensed">
-//                   <div>
-//                     <h6 class="my-0">Product name</h6>
-//                     <small class="text-muted">Item Name</small>
-//                   </div>
-//                   <span class="text-muted">$12</span>
-//                 </li>
-//                 </ul>
-//               `;
-//             }
-//           });
-//         cartItemsContainer.innerHTML = innerHTML;
-//     } catch (error) {
-//         console.error("Error fetching cart items:", error);
-//     }
-// }
+ 
    
-    
-
-
 
 // async function GetTransactions(){
 //     try{
