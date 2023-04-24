8302f0b1c82d445bec4c3fb7872c1ef5a87573d5
Amanda Malosh <amandamalosh@Amandas-MacBook-Air-2.local>
2023-04-23 21:33:49 -0500

    amanda shop update

--- Customer HTMLs/resources/scripts/sell.js
+++ Customer HTMLs/resources/scripts/sell.js
@@ -2,143 +2,162 @@ itemUrl = "http://localhost:5165/item";
 consignmentUrl = "http://localhost:5165/consignment";
 transactionUrl = "http://localhost:5165/transaction";
 adminUrl = "http://localhost:5165/admin";
-customerUrl = "http://localhost:5165/Customer";
+customerUrl = "http://localhost:5165/api/Customer";
 
 function calculateConsignment() {
-    
-  const firstNameInput = document.getElementById('firstName').value;
-  const lastNameInput = document.getElementById('lastName').value;
-  const emailInput = document.getElementById('emailInput').value;
-  const typeInput = document.getElementById('itemType').value;
-  const sizeInput = document.getElementById('sizeInput').value;
-  const qtyInput = document.getElementById('qtyInput').value;
-  const pictureInput = document.getElementById('pictureUpload').value;
+    
+  const firstNameInput = document.getElementById('firstName').value;
+  const lastNameInput = document.getElementById('lastName').value;
+  const emailInput = document.getElementById('emailInput').value;
+  const typeInput = document.getElementById('itemType').value;
+  const sizeInput = document.getElementById('sizeInput').value;
+  const qtyInput = Number(document.getElementById('qtyInput').value);
+  const pictureInput = document.getElementById('pictureUpload').value;
 
-  const price = findPrice(typeInput, qtyInput);
-  console.log(price);
+  console.log(qtyInput)
+  console.log(typeInput)
 
-  alert("The total value of the consignment is $" + price.toFixed(2));
+  const price = findPrice(typeInput, qtyInput);
+  console.log()
+  console.log(price);
+
+  alert("The total value of the consignment is $" + price.toFixed(2));
 }
 
 function findPrice(type, qty) {
-  let price = 0;
+  let price = 0;
+  console.log(price)
+  console.log(type)
+  console.log(qty)
 
-  if (type === 'shirt' || type === 'pant' || type === 'skirt') {
-    price = 5 * qty;
-  } else if (type === 'dress' || type === 'belt') {
-    price = 7 * qty;
-  } else if (type === 'jacket' || type === 'shoe') {
-    price = 10 * qty;
-  }
-
-  return price;
+  if (type === 'shirt' || type === 'pant' || type === 'skirt') {
+    price = 5 * qty;
+  } else if (type === 'dress' || type === 'belt') {
+    price = 7 * qty;
+  } else if (type === 'jacket' || type === 'shoe') {
+    price = 10 * qty;
+  }
+  updateStoreCredit(price)
+  return price;
 }
 
 const calculateButton = document.getElementById('calculateConsignment');
 
 if (calculateButton) {
-  calculateButton.addEventListener('click', calculateConsignment);
-  updateStoreCredit();
+  calculateButton.addEventListener('click', calculateConsignment());
+  // updateStoreCredit(price);
 } else {
-  console.error('Error: Could not find calculateButton element');
+  console.error('Error: Could not find calculateButton element');
 }
 
 
 function updateStoreCredit(price) {
-  
-    let activeUser = JSON.parse(localStorage.getItem("activeUser"))
-    activeUser.storeCredit = storeCredit + price
-    // const data = {
-    //   storeCredit: price
-    // };
-    activeUser.customerId = Id
-  
-    fetch(`${customerUrl}/${activeUser.customerId}`, {
-      method: "PUT",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(activeUser, Id),
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('Store credit updated successfully:', activeUser);
-    })
-    .catch(error => {
-      console.error('There was a problem updating the store credit:', error);
-    });
-  }
-  
+
+    console.log(price)
+    let activeUser = JSON.parse(localStorage.getItem("activeUser"))
+    console.log(activeUser)
+    console.log(activeUser.storeCredit)
+    activeUser.storeCredit = (activeUser.storeCredit + price)
+
+    console.log(activeUser.storeCredit)
+    // const data = {
+    //   storeCredit: price
+    // };
+    let Id = activeUser.customerId
+  
+    fetch(`${customerUrl}/${Id}`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(activeUser, Id),
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+    // .then(response => {
+    //   if (!response.ok) {
+    //     throw new Error('Network response was not ok');
+    //   }
+    //   return response.json();
+    // })
+    // .then(data => {
+    //   console.log('Store credit updated successfully:', activeUser);
+    // })
+    // .catch(error => {
+    //   console.error('There was a problem updating the store credit:', error);
+    // });
+  }
+  
 
 // function AddAnotherItem()
 // {
-//     console.log('hi')
-//     const type =
-//     {
-//         'Shirt':'shirt',
-//         'Pants' : 'pant',
-//         'Skirts' : 'skirt',
-//         'Dresses' : 'dress',
-//         'Jackets' : 'jacket',
-//         'Shoes' : 'shoes',
-//         'Belt' : 'belt',
-//     }
-//     document.addEventListener('DOMContentLoaded', function(){
-//         const typeDropdown = document.getElementById('type-Dropdown')
-//         let form = document.createElement('form')
-        
-//         // for (let category in type)
-//         // {
-//         // let option = document.createElement("option")
-//         // option.setAttribute('value', type[category])
-//         // let optionText = document.createTextNode(category)
-//         // option.appendChild(optionText)
-//         // typeDropdown.appendChild(option)
-//         // }
-   
+//     console.log('hi')
+//     const type =
+//     {
+//         'Shirt':'shirt',
+//         'Pants' : 'pant',
+//         'Skirts' : 'skirt',
+//         'Dresses' : 'dress',
+//         'Jackets' : 'jacket',
+//         'Shoes' : 'shoes',
+//         'Belt' : 'belt',
+//     }
+//     document.addEventListener('DOMContentLoaded', function(){
+//         const typeDropdown = document.getElementById('type-Dropdown')
+//         let form = document.createElement('form')
+        
+//         // for (let category in type)
+//         // {
+//         // let option = document.createElement("option")
+//         // option.setAttribute('value', type[category])
+//         // let optionText = document.createTextNode(category)
+//         // option.appendChild(optionText)
+//         // typeDropdown.appendChild(option)
+//         // }
+   
 
-//     let textInput = document.createElement('input')
-//     textInput.type = 'text'
-//     textInput.placeholder = 'Enter Size of Item'
-//     textInput.id = 'additionalSize'
-//     form.appendChild(textInput)
+//     let textInput = document.createElement('input')
+//     textInput.type = 'text'
+//     textInput.placeholder = 'Enter Size of Item'
+//     textInput.id = 'additionalSize'
+//     form.appendChild(textInput)
 
-//     let textInput1 = document.createElement('input')
-//     textInput1.type = 'text'
-//     textInput1.placeholder = 'Enter picture link'
-//     textInput1.id = 'additionalPic'
-//     form.appendChild(textInput1)
+//     let textInput1 = document.createElement('input')
+//     textInput1.type = 'text'
+//     textInput1.placeholder = 'Enter picture link'
+//     textInput1.id = 'additionalPic'
+//     form.appendChild(textInput1)
 
-//     let submitButton = document.createElement('button')
-//     submitButton.textContent = 'Submit'
-//     form.appendChild(submitButton)
+//     let submitButton = document.createElement('button')
+//     submitButton.textContent = 'Submit'
+//     form.appendChild(submitButton)
 
-//     let consignmentCount = '1'
-//     form.addEventListener('submit', function(e){
-//         e.preventDefault()
-//         let consignment = {
-//             consignmentId: consignmentCount ++,
-//             size: e.target.elements.additionalSize.value,
-//             pictureLink: e.target.elements.additionalPic.value,
-            
-//         }
-        
-//         addRow(consignment)
+//     let consignmentCount = '1'
+//     form.addEventListener('submit', function(e){
+//         e.preventDefault()
+//         let consignment = {
+//             consignmentId: consignmentCount ++,
+//             size: e.target.elements.additionalSize.value,
+//             pictureLink: e.target.elements.additionalPic.value,
+            
+//         }
+        
+//         addRow(consignment)
 
-//         e.target.elements.additionalType = ''
-//         e.target.elements.additionalSize = ''
-//         e.target.elements.additionalPic = ''
+//         e.target.elements.additionalType = ''
+//         e.target.elements.additionalSize = ''
+//         e.target.elements.additionalPic = ''
 
-//     })
-    
+//     })
+    
 
-//     app.appendChild(form)
+//     app.appendChild(form)
 // });
 // }
 
@@ -148,5 +167,5 @@ function updateStoreCredit(price) {
 // var form = document.getElementById("sellForm");
 
 // document.getElementById("sellForm").addEventListener("click", function () {
-//   form.submit();
+//   form.submit();
 // });
